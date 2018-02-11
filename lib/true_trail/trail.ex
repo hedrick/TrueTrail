596bfba85def8f1d776a722e3b9b7336c00d81fb
Scott Hedrick <me@scottshedrick.com>
2018-02-10 18:28:04 -0600

    updated Trail and tests

--- lib/true_trail/trail.ex
+++ lib/true_trail/trail.ex
@@ -4,16 +4,6 @@ defmodule TrueTrail.Trail do
   @enforce_keys [:date, :info]
   defstruct [:date, :info]
 
-  @doc ~S"""
-  Generates a new Trail instance with a Date, :date and MapSet, :info
-
-  ## Examples
-
-      iex> {:ok date} = Date.new(2018, 2, 10)
-      iex> Trail.new(date, nil)
-      %TrueTrail.Trail{date: ~D[2018-02-10], info: nil}
-
-  """
   def new(date, info), do:
     %Trail{date: date, info: info}
 
--- test/true_trail_test.exs
+++ test/true_trail_test.exs
@@ -2,4 +2,10 @@ defmodule TrueTrailTest do
   use ExUnit.Case
   doctest TrueTrail
 
+  test "new trail created" do
+    {:ok, date} = Date.new(2018, 2, 10)
+    assert TrueTrail.Trail.new(date, nil) == 
+    %TrueTrail.Trail{date: ~D[2018-02-10], info: nil}
+  end
+
 end
